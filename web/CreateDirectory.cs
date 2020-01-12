a9ede60a03f62e0fec7c441cbb2f7149e1c59d26
郝晓东 <vistasla@163.com>
2020-01-12 16:53:20 +0800

    first commit

---
+++ web/CreateDirectory.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+namespace webspider
+{
+    class CreateDirectory
+    {
+        public CreateDirectory(string strDirectoryPath)
+        {
+
+            Directory.CreateDirectory(strDirectoryPath);
+
+        }
+    }
+}
