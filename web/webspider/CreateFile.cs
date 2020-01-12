7042910d88748ca4c33d7a7b82948b0a876e7856
郝晓东 <vistasla@163.com>
2020-01-12 20:48:22 +0800

    test

---
+++ web/webspider/CreateFile.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+namespace webspider
+{
+    class CreateFile
+    {
+        public CreateFile(string strFilePath)
+        {
+
+            FileStream fs = File.Create(strFilePath);
+            fs.Close();
+        }
+    }
+}
---
+++ web/webspider/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace webspider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Boolean[] booleanArray = new Boolean[5];
+            string strUrl = textBox1.Text;
+            ComputeCategoryCount ccc = new ComputeCategoryCount(strUrl);
+            //ParseHtmlDocument phd = new ParseHtmlDocument(strUrl, booleanArray);
+            //GetCategoryUrl gcu = new GetCategoryUrl(strUrl);
+        }
+    }
+}
